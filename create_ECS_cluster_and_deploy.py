70733e553990be571ff2fba68b00267e07e78bcf
uzairash <uzairashfaq90@gmail.com>
2024-12-25 17:15:17 +0500

    issue depends_on

--- create_ECS_cluster_and_deploy.py
+++ create_ECS_cluster_and_deploy.py
@@ -9,12 +9,12 @@ def create_cluster(ecs_client, cluster_name):
         capacityProviders=['FARGATE'],
         serviceConnectDefaults={
         'namespace': cluster_name
-    }
+        }
     )
     print(f'Cluster {cluster_name} created successfully')
     return response['cluster']['clusterArn']
 
-def register_task_definition(ecs_client, service_name, image, environment, port_mappings, private_repo_creds, execution_role, task_role):
+def register_task_definition(ecs_client, service_name, image, environment, port_mappings :list, private_repo_creds, execution_role, task_role):
     response = ecs_client.register_task_definition(
         family=service_name,
         containerDefinitions=[
@@ -28,7 +28,7 @@ def register_task_definition(ecs_client, service_name, image, environment, port_
                 'memory': 512,
                 'essential': True,
                 'environment': [{'name': k, 'value': v} for k, v in environment.items()],
-                'portMappings': port_mappings
+                'portMappings': [{**port_mappings[0], 'name': service_name}] if port_mappings else [{'name': service_name}]
             }
         ],
         ephemeralStorage={
@@ -48,13 +48,14 @@ def register_task_definition(ecs_client, service_name, image, environment, port_
     print(f'Task definition for {service_name} created successfully')
     return response['taskDefinition']['taskDefinitionArn']
 
-def create_service(ecs_client, cluster_name, service_name, task_definition, subnets, security_groups):
+def create_service(ecs_client, cluster_name, service_name, task_definition, subnets, security_groups, port_mappings):
     response = ecs_client.create_service(
         cluster=cluster_name,
         serviceName=service_name,
         taskDefinition=task_definition,
         desiredCount=1,
         launchType='FARGATE',
+        enableExecuteCommand=True,
         networkConfiguration={
             'awsvpcConfiguration': {
                 'subnets': subnets,
@@ -64,8 +65,19 @@ def create_service(ecs_client, cluster_name, service_name, task_definition, subn
         },
         serviceConnectConfiguration={
             'enabled': True,
-            'namespace': cluster_name,
-            
+            'namespace': cluster_name, 
+            'services': [
+                {
+                    'portName': service_name,
+                    'discoveryName': service_name,
+                    'clientAliases': [
+                        {
+                            'port': port_mappings[0]['containerPort'],
+                            'dnsName': service_name
+                        },
+                    ]
+                }
+            ]    
         }
     )
     print(f'Service {service_name} created successfully')
@@ -74,8 +86,9 @@ def create_service(ecs_client, cluster_name, service_name, task_definition, subn
 
 
 # Cluster details
-cluster_name = 'microservices-cluster'
-subnets = ['subnet-021e4c85656747d98', 'subnet-091f161d52ffc95ac', 'subnet-0f589277124822a92']
+cluster_name = 'microsvc-cluster-11'
+#subnets = ['subnet-021e4c85656747d98', 'subnet-091f161d52ffc95ac', 'subnet-0f589277124822a92']
+subnets = ['subnet-021e4c85656747d98']
 security_groups = ['sg-0f7d983d4dc5cd3e2']
 execution_role_arn = 'arn:aws:iam::730335323304:role/ecsTaskExecutionRole'
 task_role_arn = 'arn:aws:iam::730335323304:role/ecsTaskExecutionRole'
@@ -86,74 +99,74 @@ cluster_arn = create_cluster(ecs_client, cluster_name)
 
 # Microservices configuration
 services = [
-    {
-        'name': 'frontend',
-        'image': 'uzair102/u_repo:frontend',
-        'environment': {
-            'PORT': '8080',
-            'AUTH_API_ADDRESS': 'http://auth-api:8081',
-            'TODOS_API_ADDRESS': 'http://todos-api:8082',
-            'ZIPKIN_URL': 'http://zipkin:9411/api/v2/spans'
-        },
-        'portMappings': [{'containerPort': 8080, 'hostPort': 8080, 'protocol': 'tcp'}]
-    },
-    {
-        'name': 'auth-api',
-        'image': 'uzair102/u_repo:auth-api',
-        'environment': {
-            'AUTH_API_PORT': '8081',
-            'JWT_SECRET': 'myfancysecret',
-            'USERS_API_ADDRESS': 'http://users-api:8083',
-            'ZIPKIN_URL': 'http://zipkin:9411/api/v2/spans'
-        },
-        'portMappings': [{'containerPort': 8081, 'hostPort': 8081, 'protocol': 'tcp'}]
-    },
-    {
-        'name': 'todos-api',
-        'image': 'uzair102/u_repo:todos-api',
-        'environment': {
-            'TODO_API_PORT': '8082',
-            'JWT_SECRET': 'myfancysecret',
-            'REDIS_HOST': 'redis-queue',
-            'REDIS_PORT': '6379',
-            'REDIS_CHANNEL': 'log_channel',
-            'ZIPKIN_URL': 'http://zipkin:9411/api/v2/spans'
-        },
-        'portMappings': [{'containerPort': 8082, 'hostPort': 8082, 'protocol': 'tcp'}]
-    },
-    {
-        'name': 'users-api',
-        'image': 'uzair102/u_repo:users-api',
-        'environment': {
-            'SERVER_PORT': '8083',
-            'JWT_SECRET': 'myfancysecret',
-            'SPRING_ZIPKIN_BASE_URL': 'http://zipkin:9411'
-        },
-        'portMappings': [{'containerPort': 8083, 'hostPort': 8083, 'protocol': 'tcp'}]
-    },
+    # {
+    #     'name': 'frontend',
+    #     'image': 'uzair102/u_repo:frontend',
+    #     'environment': {
+    #         'PORT': '8080',
+    #         'AUTH_API_ADDRESS': 'http://auth-api:8081',
+    #         'TODOS_API_ADDRESS': 'http://todos-api:8082',
+    #         'ZIPKIN_URL': 'http://zipkin:9411/api/v2/spans'
+    #     },
+    #     'portMappings': [{'containerPort': 8080, 'hostPort': 8080, 'protocol': 'tcp', 'appProtocol': 'http'}]
+    # },
+    # {
+    #     'name': 'auth-api',
+    #     'image': 'uzair102/u_repo:auth-api',
+    #     'environment': {
+    #         'AUTH_API_PORT': '8081',
+    #         'JWT_SECRET': 'myfancysecret',
+    #         'USERS_API_ADDRESS': 'http://users-api:8083',
+    #         #'ZIPKIN_URL': 'http://zipkin:9411/api/v2/spans'
+    #     },
+    #     'portMappings': [{'containerPort': 8081, 'hostPort': 8081, 'protocol': 'tcp', 'appProtocol': 'http'}]
+    # },
+    # {
+    #     'name': 'todos-api',
+    #     'image': 'uzair102/u_repo:todos-api',
+    #     'environment': {
+    #         'TODO_API_PORT': '8082',
+    #         'JWT_SECRET': 'myfancysecret',
+    #         'REDIS_HOST': 'redis-queue',
+    #         'REDIS_PORT': '6379',
+    #         'REDIS_CHANNEL': 'log_channel',
+    #         'ZIPKIN_URL': 'http://zipkin:9411/api/v2/spans'
+    #     },
+    #     'portMappings': [{'containerPort': 8082, 'hostPort': 8082, 'protocol': 'tcp', 'appProtocol': 'http'}]
+    # },
+    # {
+    #     'name': 'users-api',
+    #     'image': 'uzair102/u_repo:users-api',
+    #     'environment': {
+    #         'SERVER_PORT': '8083',
+    #         'JWT_SECRET': 'myfancysecret',
+    #         'SPRING_ZIPKIN_BASE_URL': 'http://zipkin:9411'
+    #     },
+    #     'portMappings': [{'containerPort': 8083, 'hostPort': 8083, 'protocol': 'tcp', 'appProtocol': 'http'}]
+    # },
     {
         'name': 'log-message-processor',
-        'image': 'log-message-processor',
+        'image': 'uzair102/u_repo:log-message-processor',
         'environment': {
             'REDIS_HOST': 'redis-queue',
             'REDIS_PORT': '6379',
             'REDIS_CHANNEL': 'log_channel',
             'ZIPKIN_URL': 'http://zipkin:9411/api/v1/spans'
         },
-        'portMappings': []
+        'portMappings': [{'containerPort': 6329, 'hostPort': 6329, 'protocol': 'tcp', 'appProtocol': 'http'}]
     },
-    {
-        'name': 'zipkin',
-        'image': 'openzipkin/zipkin',
-        'environment': {},
-        'portMappings': [{'containerPort': 9411, 'hostPort': 9411, 'protocol': 'tcp'}]
-    },
-    {
-        'name': 'redis-queue',
-        'image': 'redis',
-        'environment': {},
-        'portMappings': []
-    }
+    # {
+    #     'name': 'zipkin',
+    #     'image': 'openzipkin/zipkin',
+    #     'environment': {},
+    #     'portMappings': [{'containerPort': 9411, 'hostPort': 9411, 'protocol': 'tcp', 'appProtocol': 'http'}]
+    # },
+    # {
+    #     'name': 'redis-queue',
+    #     'image': 'redis',
+    #     'environment': {},
+    #     'portMappings': [{'containerPort': 6379, 'hostPort': 6379, 'protocol': 'tcp', 'appProtocol': 'http'}]
+    # }
 ]
 
 # Deploy services
@@ -174,7 +187,8 @@ for service in services:
         service_name=service['name'],
         task_definition=task_def_arn,
         subnets=subnets,
-        security_groups=security_groups
+        security_groups=security_groups,
+        port_mappings=service['portMappings'] if 'portMappings' in service else []
     )
 
 print("All services deployed successfully.")
